dffa0800f8f0243deda728d2a10ef5f3a3b0ad6a
Chrysellia <priscilliachryso@gmail.com>
2022-02-22 17:37:45 +0300

    Portfolio updated

--- myportfolio/src/pages/components/Navbar/Navbar.component.js
+++ myportfolio/src/pages/components/Navbar/Navbar.component.js
@@ -4,8 +4,8 @@ import './Navbar.style.scss'
 const Navbar = () => {
   return (
     <div className="navbar-component">
-        <div className="navbar-wrapper">
-            <div className="navbar-name">Priscillia</div>
+      <div className="navbar-wrapper">
+          <div className="navbar-name">Priscillia</div>
             <div className="navbar-menu">
                 <div className="menu-item">Accueil</div>
                 <div className="menu-item">A propos</div>
--- myportfolio/src/pages/components/Portfolio/Portfolio.component.js
+++ myportfolio/src/pages/components/Portfolio/Portfolio.component.js
@@ -6,7 +6,30 @@ const Portfolio = () => {
     <div className="portfolio-component">
         <div className="portfolio-wrapper">
             <div className="portfolio-title">Portfolio</div>
-            
+            <div className="portfolio-gallery">
+              <div className="gallery-wrapper">
+                <img src="images/portfolio/roompage.png"/>
+              </div>
+              <div className="gallery-wrapper">
+                <img src="images/portfolio/ecommerce.png"/>
+              </div>
+              <div className="gallery-wrapper">
+                <img src="images/portfolio/management.png"/>
+              </div>
+
+              <div className="gallery-wrapper">
+                <img src="images/portfolio/sunnyside.png"/>
+              </div>
+              <div className="gallery-wrapper">
+                <img src="images/portfolio/shorterlink.png"/>
+              </div>
+              <div className="gallery-wrapper">
+                <img src="images/portfolio/timetracking.png"/>
+              </div>
+              <div className="gallery-wrapper">
+                <img src="images/portfolio/dashboard.png"/>
+              </div>
+            </div>
         </div>
     </div>
   )
